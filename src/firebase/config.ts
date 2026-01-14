9e0616401dfad5eed558dbfa6fd88c7ea7f4c67b
Vibha Shankar Rao <vibhashankar1802@gmail.com>
2026-01-14 06:59:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1996566756-dfcbb",
+  "appId": "1:71925889846:web:a7bd7dc1cfb0eae3151197",
+  "apiKey": "AIzaSyCSjrPp4mk6zCkWthBaEH6OJQDQJWtBFpg",
+  "authDomain": "studio-1996566756-dfcbb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "71925889846"
+};
